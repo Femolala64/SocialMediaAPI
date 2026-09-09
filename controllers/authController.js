9a8bb4456eeb5138ac873ec80246775503cee955
Femolala64 <Oluwafemigabriel33@gmail.com>
2026-09-10 00:35:29 +0100

    feat: add user signin with credential validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -58,4 +58,49 @@ const signup = async (req, res) => {
   }
 };
 
-module.exports = { signup };
+const signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        status: false,
+        message: "Invalid credentials",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        status: false,
+        message: "Invalid credentials ",
+      });
+    }
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        status: false,
+        message: "Invalid credentials",
+      });
+    }
+    const token = generateToken(user._id);
+    return res.status(200).json({
+      status: true,
+      message: "Login Successful",
+      data: {
+        id: user._id,
+        email: user.email,
+        username: user.username,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = { signup, signin };
