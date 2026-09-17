3d71d94d7700ceaf72bdb0f9649cc27d67414d01
Femolala64 <Oluwafemigabriel33@gmail.com>
2026-09-17 13:05:53 +0100

    add in-memory mongo test setup

---
+++ tests/setup.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+
+let mongo;
+
+beforeAll(async () => {
+  mongo = await MongoMemoryServer.create();
+  const uri = mongo.getUri();
+  await mongoose.connect(uri);
+}, 60000);
+
+afterEach(async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    await collections[key].deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongo.stop();
+});
